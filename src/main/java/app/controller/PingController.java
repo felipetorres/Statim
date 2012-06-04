cdf65e331d24574824e8d058a25442b793c108de
Felipe Torres <felipe.gtorres@gmail.com>
2012-06-04 20:25:54 -0300

    ...

--- src/main/java/app/controller/PingController.java
+++ src/main/java/app/controller/PingController.java
@@ -3,12 +3,15 @@ package app.controller;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.URL;
+import java.util.List;
 
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLSession;
 
 import app.dao.SenderDao;
+import app.model.Device;
+import app.model.Sender;
 import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Resource;
 
@@ -22,22 +25,22 @@ public class PingController {
 	}
 	
 	@Path("/ping/{email}")
-	public int ping(String email) throws IOException {
-//		Sender sender = senders.findByEmail(email);
-//		List<Device> devices = senders.findDevicesOf(sender);
-//		System.out.println("Ping...");
-//		for (Device device : devices) {
-//			try {
-//				return pingDevice(sender, device);
-//			} catch (IOException e) {
-//				e.printStackTrace();
-//			}
-//		}
-		return pingDevice();
+	public int ping(String email) {
+		Sender sender = senders.findByEmail(email);
+		List<Device> devices = senders.findDevicesOf(sender);
+		System.out.println("Ping...");
+		for (Device device : devices) {
+			try {
+				return pingDevice(sender, device);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return 0;
 	}
 
-	private int pingDevice() throws IOException {
-		String data = "registration_id=" + "APA91bE1bRIk33acREVQFVT0UzBJNS_3H_WyJZUjdT4Vcb0s4o2wNmUas3xQxU1cS5qT-2zUjod8sqAEmsn6my2KdSyFEcgUohpY3b3auJ6rkEXeDzYC0Gn5bOr9gbz5fFasEPNbi5P1OIB1RxK_eTSuFbCJgL5u5g" 
+	private int pingDevice(Sender sender, Device device) throws IOException {
+		String data = "registration_id=" + device.getRegistrationId() 
 					+ "&collapse_key=0"
 					+ "&data.message=gps";
 		
@@ -49,7 +52,7 @@ public class PingController {
 		connection.setRequestMethod("POST");
 		connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded;charset=UTF=8");
 		connection.setRequestProperty("Content-Length",Integer.toString(data.length()));
-		connection.setRequestProperty("Authorization", "GoogleLogin auth=" + "DQAAAMIAAADGTDhDS1Rr8gEBYj_Ed-ItH9nTdYr0Z62mO-W713roY-tOrMbBpCyRkfkwyuQqiSmx1wQtsQD0pXxLht7QFVkLZUzzhC2G_sModlN10MahxcC9NoTuT0yjDDEtGVIRa3a5ewLYM97hnFbKGMzAvjumYirNLSFKK1n1Hv5t6j5tv8_pZ9n_LKXqpA9MBd0fxnzMdjHu4Q_IpIWsv0tVRynuCGH_f3sfG1LImU1Ce75NpGVOSkSIX7opl8JmAmHOBogqiJHGK2hXlGL7JZVKnrdO");
+		connection.setRequestProperty("Authorization", "GoogleLogin auth=" + sender.getHash());
 		OutputStream out = connection.getOutputStream();
 		out.write(data.getBytes());
 		out.close();
