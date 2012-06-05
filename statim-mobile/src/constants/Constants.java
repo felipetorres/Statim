edead9d73db4de1ae75c89700e7270f63f510364
Felipe Torres <felipe.gtorres@gmail.com>
2012-06-05 19:48:46 -0300

    Removendo client da branch server...

--- statim-mobile/src/constants/Constants.java
+++
@@ -1,9 +0,0 @@
-package constants;
-
-public class Constants {
-
-	public final static String settings_key = "old_registration_id";
-	public final static String settings_filename = "statim";
-	public final static String sender_email = "felipe.gtorres@gmail.com";
-	public final static String server_url = "http://192.168.84.130:8080/statim-server";
-}
