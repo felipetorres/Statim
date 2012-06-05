9c5b5cc9f62f463a07e0842e87ac300970a4fc92
Felipe Torres <felipe.gtorres@gmail.com>
2012-06-05 18:21:04 -0300

    Teste bobo.

---
+++ src/test/java/app/controller/SenderControllerTest.java
@@ -0,0 +1,30 @@
+package app.controller;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.verify;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import app.dao.SenderDao;
+import app.model.Sender;
+
+public class SenderControllerTest {
+
+	private @Mock SenderDao senders;
+	private SenderController controller;
+
+	@Before
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+		controller = new SenderController(senders);
+	}
+	
+	@Test
+	public void shouldSaveSender() throws Exception {
+		controller.save("sender@email.com", "aSenderHash");
+		verify(senders).save(any(Sender.class));
+	}
+}
